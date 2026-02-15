bf6504fb158a3fbb41aef31dc6850c43a5cf96dc
KPN2413 <kprakharnarayan04@gmail.com>
2026-02-15 16:05:00 +0530

    step 6.2 add category entity

---
+++ apps/api/src/categories/category.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+} from 'typeorm';
+
+@Entity({ name: 'categories' })
+@Index(['name'], { unique: true })
+@Index(['slug'], { unique: true })
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', length: 120 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 140 })
+  slug!: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt!: Date;
+}
