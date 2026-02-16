5f6425bc8b011e236694b0d81473f77dd600dec0
KPN2413 <kprakharnarayan04@gmail.com>
2026-02-16 13:50:06 +0530

    step 6.2 add POST /categories

--- apps/api/src/categories/categories.controller.ts
+++ apps/api/src/categories/categories.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  create(@Body() dto: CreateCategoryDto) {
+    return this.categoriesService.create(dto);
+  }
 }
